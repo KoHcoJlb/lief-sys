cb55824277accbf8d38ec5492f21ea27aee06855
Ihor Stuzhuk <fujitsuigor@gmail.com>
2025-02-22 13:16:49 +0200

    Static C++

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 fn main() -> miette::Result<()> {
     let mut cmake_config = cmake::Config::new("LIEF");
     for opt in [
@@ -6,22 +8,29 @@ fn main() -> miette::Result<()> {
         "LIEF_PE",
         "LIEF_MACHO",
         "LIEF_DEX",
-        "LIEF_ART"
+        "LIEF_ART",
     ] {
         cmake_config.define(opt, "OFF");
     }
     let lief_path = cmake_config.build();
 
-    cxx_build::bridge("src/lib.rs")
+    let mut build = cxx_build::bridge("src/lib.rs");
+    build
         .include("src")
         .include(lief_path.join("include"))
-        .std("c++20")
-        .compile("lief-sys");
+        .std("c++20");
+    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "android" {
+        build.cpp_link_stdlib("c++_static");
+    }
+    build.compile("lief-sys");
 
     println!("cargo::rerun-if-changed=src/lib.hpp");
     println!("cargo::rerun-if-changed=src/lib.rs");
 
-    println!("cargo::rustc-link-search={}", lief_path.join("lib").to_str().unwrap());
+    println!(
+        "cargo::rustc-link-search={}",
+        lief_path.join("lib").to_str().unwrap()
+    );
     println!("cargo::rustc-link-lib=LIEF");
 
     Ok(())
